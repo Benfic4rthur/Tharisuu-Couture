c2466a7b37a5725c28f69f739eabf9925aae9160
Benfi4rthur <arthur_benfica@hotmail.com>
2023-05-05 15:27:52 -0300

    audio hover

--- js/modal.js
+++ js/modal.js
@@ -2,3 +2,7 @@ function abrirModal(caminhoDaImagem) {
   document.getElementById("imagemModalImg").src = caminhoDaImagem;
   $("#imagemModal").modal("show");
 }
+function playHoverSound() {
+  var hoverSound = document.getElementById("hover-sound");
+  hoverSound.play();
+}
